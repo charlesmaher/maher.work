927ecb8a717040036e3bce85482a2c9fec713a5e
Charles Maher <46153209+charlesmaher@users.noreply.github.com>
2022-09-15 00:10:14 +1000

    Create a SvelteKit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<div id="container">
+    <h1>charles|maher</h1>
+    <span>
+        <h2>there is nothing here</h2>
+        <p>(yet)</p>
+    </span>
+    <p>made with SvelteKit</p>
+</div>
+
+<style>
+    :global(html, body) {
+        margin: 0;
+    }
+
+    :root {
+        font-family: sans-serif;
+        color: white;
+        background: black;
+        text-align: center;
+    }
+
+    #container { 
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        min-height: 100vh;
+    }
+
+    p {
+        color: darkcyan;
+    }
+</style>
