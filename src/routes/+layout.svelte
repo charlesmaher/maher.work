bd6139914963286d9ded588ed7be1536ed3567f3
Charles Maher <46153209+charlesmaher@users.noreply.github.com>
2022-10-10 00:57:14 +1100

    Use correct URL replacement method in layout Also updated package.json

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
+<script lang="ts">
+    import { assets } from "$app/paths";
+</script>
+
 <svelte:head>
     <title>charles.maher</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="%sveltekit.assets%/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="%sveltekit.assets%/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="%sveltekit.assets%/favicon-16x16.png">
-    <link rel="manifest" href="%sveltekit.assets%/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="{assets}/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{assets}/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{assets}/favicon-16x16.png">
+    <link rel="manifest" href="{assets}/site.webmanifest">
 </svelte:head>
 
 <slot></slot>
