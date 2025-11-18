dcf63b821084cae1b41a214e48dfaba3998b77d1
Charles Maher <git.charles@maher.work>
2025-11-18 18:07:32 +1100

    Initial homepage implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main
+	class="flex h-screen min-h-100 flex-col flex-wrap text-center font-mono text-4xl text-white lowercase md:flex-row"
+>
+	<div
+		class="flex flex-1 items-center justify-center bg-[url(/img/digital.jpg)] bg-cover bg-center text-[#00ff00]"
+	>
+		<a href="/digital">
+			<div
+				class="rounded-xl border-4 border-[#00ff00] bg-black/50 px-20 py-10 shadow-lg shadow-black backdrop-blur-xs transition hover:bg-black/20 hover:backdrop-blur-lg"
+			>
+				Digital
+			</div>
+		</a>
+	</div>
+	<div class="flex flex-1 items-center justify-center bg-[url(/img/analog.jpg)] bg-cover bg-center">
+		<a href="/analog">
+			<div
+				class="rounded-xl border-4 bg-black/50 px-20 py-10 shadow-lg shadow-black backdrop-blur-xs transition hover:bg-black/20 hover:backdrop-blur-lg"
+			>
+				Analog
+			</div>
+		</a>
+	</div>
+	<div class="absolute top-10 flex w-full justify-center text-5xl">
+		<span class="text-right md:flex-1">Charles</span>&nbsp;<span
+			class="text-left md:flex-1 md:text-purple-950">Maher</span
+		>
+	</div>
+</main>
