5cb0d502e9505cd1163ff4c61c32de7d841351ad
Charles Maher <git.charles@maher.work>
2025-11-23 18:37:26 +1100

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+	<title>Charles Maher</title>
+</svelte:head>
+
 <main
 	class="flex h-screen min-h-100 flex-col flex-wrap text-center font-mono text-4xl text-white lowercase md:flex-row"
 >
@@ -12,7 +16,9 @@
 			</div>
 		</a>
 	</div>
-	<div class="flex flex-1 items-center justify-center bg-[url(/img/analog.jpg)] bg-cover bg-center">
+	<div
+		class="flex flex-1 items-center justify-center bg-[url(/img/analog.jpg)] bg-cover bg-center font-sans"
+	>
 		<a href="/analog">
 			<div
 				class="rounded-xl border-4 bg-black/50 px-20 py-10 shadow-lg shadow-black backdrop-blur-xs transition hover:bg-black/20 hover:backdrop-blur-lg"
@@ -23,7 +29,7 @@
 	</div>
 	<div class="absolute top-10 flex w-full justify-center text-5xl">
 		<span class="text-right md:flex-1">Charles</span>&nbsp;<span
-			class="text-left md:flex-1 md:text-purple-950">Maher</span
+			class="text-left md:flex-1 md:font-sans md:text-purple-950">Maher</span
 		>
 	</div>
 </main>
